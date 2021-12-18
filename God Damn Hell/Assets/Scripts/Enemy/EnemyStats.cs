674f8ea59d570d93d0dde873631661bbab729f40
MarhalmGH <93052627+MarhalmGH@users.noreply.github.com>
2021-12-18 03:40:05 +0100

    EnemyMovement update to NavMesh

--- God Damn Hell/Assets/Scripts/Enemy/EnemyStats.cs
+++ God Damn Hell/Assets/Scripts/Enemy/EnemyStats.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class EnemyStats : MonoBehaviour
 {
     // Once actual enemies exist this should be changed to protected
-    public int healthPoints = 10;
+    public int healthPoints = 5;
 
     public float movementspeed = 2.5f;
 
-    public float aggroRange;
+    public float aggroRange = 10;
 }
